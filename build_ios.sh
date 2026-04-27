92e79539873f5cac35d1c6d7a677ec3ccb48123f
yaoxiaobing <yaoxiaobing@360.cn>
2026-04-27 16:47:33 +0800

    doc: update build_ios.sh

--- build_ios.sh
+++ build_ios.sh
@@ -5,13 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)";
 cd $SCRIPT_DIR
 
 sh init-ios.sh
-# sh init-ios-openssl.sh
 
 cd $SCRIPT_DIR/ios
-
-# build openssl first
-# sh compile-openssl.sh clean
-# sh compile-openssl.sh all
-# build ffmpeg with openssl
+# build ffmpeg with ios securetransport
 sh compile-ffmpeg.sh all
 
